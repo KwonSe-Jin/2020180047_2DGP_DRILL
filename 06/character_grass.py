54af70fb391385727f8d3f92db0208d2506268d7
KwonSe-Jin <sejin6849@gmail.com>
2022-09-21 11:28:49 +0900

    DRILL 06
    2020180047 권세진

---
+++ 06/character_grass.py
@@ -0,0 +1,47 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 0
+while (x < 800):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x,90)
+    x = x + 2
+    delay(0.001)
+    
+y=0
+while (y < 800):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(800, y)
+    y = y + 2
+    delay(0.001)
+
+x = 800
+while (x > 0):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, 600)
+    x = x - 2
+    delay(0.001)
+
+y=600
+while (y > 90):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(0, y)
+    y = y - 2
+    delay(0.001)
+
+x
+
+
+
+
+
+close_canvas()
